2414c0ee2e73470f635df9e35d401495a46d5aab
kulot <lacsamana.vir@sdca.edu.ph>
2023-12-17 15:01:40 +0800

    edit button

--- public/index.js
+++ public/index.js
@@ -18,6 +18,39 @@ function changeModalHeaderColor(status){
     }
 }
 
+function assignRowFieldvalues(row){
+    let columns     = row.getElementsByTagName('td');
+    let ticketNo    = row.getElementsByTagName('th');
+
+    let title       = document.getElementById('request-title');
+    let description = document.getElementById('ticket-description');
+    let dateCreated = document.getElementById('date-created');
+    let targetDate  = document.getElementById('target-date');
+    let requestedBy = document.getElementById('requested-by');
+    let assignedTo  = document.getElementById('assigned-to');
+    let department  = document.getElementById('department');
+    let category    = document.getElementById('category');
+    let modalTitle  = document.getElementById('modal-ticket-no');
+    let status      = document.getElementById('field-status');
+    const statusArray = ['ongoing', 'on queue' , 'completed' , 'overdue'];
+
+    title.value             = columns[0].textContent;
+    requestedBy.value       = columns[1].textContent;
+    department.value        = columns[2].textContent;
+    dateCreated.value       = columns[3].textContent;
+    targetDate.value        = columns[4].textContent;
+    category.value          = columns[5].textContent;
+    status.value            = columns[6].textContent;
+    modalTitle.textContent  = ticketNo[0].textContent;
+
+    changeModalHeaderColor(columns[6].textContent);   
+
+}
+
+function showHideModalButtons(row, state = ''){
+    
+}
+
 // MAIN CONNTENT
 
 document.addEventListener('DOMContentLoaded',function() {
@@ -32,35 +65,28 @@ document.addEventListener('DOMContentLoaded',function() {
     });
 });
     //View BUTTON
-
     viewButton = document.querySelectorAll('.view-ticket');
     viewButton.forEach(function(button){
-    button.addEventListener('click', function()
-    {
-        let row         = this.parentElement.parentElement;
-        let columns     = row.getElementsByTagName('td');
-        let ticketNo    = row.getElementsByTagName('th');
-
-        let title       = document.getElementById('request-title');
-        let description = document.getElementById('ticket-description');
-        let dateCreated = document.getElementById('date-created');
-        let targetDate  = document.getElementById('target-date');
-        let requestedBy = document.getElementById('requested-by');
-        let assignedTo  = document.getElementById('assigned-to');
-        let department  = document.getElementById('department');
-        let category    = document.getElementById('category');
-        let modalTitle  = document.getElementById('modal-ticket-no');
-
-
-        title.value = columns[0].textContent;
-        requestedBy.value = columns[1].textContent;
-        department.value = columns[2].textContent;
-        dateCreated.value = columns[3].textContent;
-        targetDate.value = columns[4].textContent;
-        category.value   = columns[5].textContent;
-        modalTitle.textContent = ticketNo[0].textContent;
-
-        changeModalHeaderColor(columns[6].textContent);
+        button.addEventListener('click', function(){
+            let row = this.parentElement.parentElement;
+            assignRowFieldvalues(row);
+        
     });
+    
+    
 });
 
+    //EDIT BUTTON
+    editButton = document.querySelectorAll('.edit-ticket');
+    editButton.forEach(function(button){
+        button.addEventListener('click', function(){
+            let row = this.parentElement.parentElement;
+            assignRowFieldvalues(row);
+
+            const inputFields = document.querySelectorAll(".form-control");
+            inputFields.forEach(input => {
+                if(input.id != "date-completed" && input.id != "field-status" && input.id != "date-created" ) input.removeAttribute("disabled");
+            
+            });
+    });
+});
